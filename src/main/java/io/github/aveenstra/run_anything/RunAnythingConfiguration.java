7fdfe33fc3d12248c317c1b6063e194d31fa0546
Veenstra <aveenstra@users.noreply.github.com>
2021-02-16 10:56:55 +0100

    Update copyright year

--- src/main/java/io/github/aveenstra/run_anything/RunAnythingConfiguration.java
+++ src/main/java/io/github/aveenstra/run_anything/RunAnythingConfiguration.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2020 A Veenstra
+ *    Copyright 2021 A Veenstra
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
